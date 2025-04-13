d5d2a6eab5f092848e3c03e2bd62243fbcb939cc
suyash1702 <ssmungase2580@gmail.com>
2025-04-13 12:29:47 +0530

    Fix TypeScript errors in server.ts

--- landing-page-app/server.ts
+++ landing-page-app/server.ts
@@ -37,8 +37,8 @@ export function app(): express.Express {
         publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+      .then((html: string) => res.send(html))
+      .catch((err: Error) => next(err));
   });
 
   return server;
