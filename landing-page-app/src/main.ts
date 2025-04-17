b7b08a78aa37dbbf650f0c79c19c491f7d4353b5
suyash1702 <ssmungase2580@gmail.com>
2025-04-18 00:44:40 +0530

    Fix route import in main.ts

--- landing-page-app/src/main.ts
+++ landing-page-app/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing.module';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
